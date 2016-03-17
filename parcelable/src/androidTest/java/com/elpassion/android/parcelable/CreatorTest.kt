b7fcfa9ecd2025aeff153165089c757f866f854b
Ordon <ordon1313@gmail.com>
2016-03-17 15:55:51 +0100

    Creator

---
+++ parcelable/src/androidTest/java/com/elpassion/android/parcelable/CreatorTest.kt
@@ -0,0 +1,34 @@
+package com.elpassion.android.parcelable
+
+import android.os.Parcel
+import android.os.Parcelable
+import org.junit.Assert.*
+import org.junit.Test
+
+class CreatorTest {
+
+    private val parcel = Parcel.obtain()
+    private val parcelable = ParcelableImpl()
+    private val createObject: Parcel.() -> ParcelableImpl = { parcelable }
+
+    @Test
+    fun shouldCreateArrayOfNulls() {
+        val CREATOR = createCreator(createObject)
+        val createdArray = CREATOR.newArray(4)
+        assertArrayEquals(arrayOfNulls(4), createdArray)
+    }
+
+    @Test
+    fun shouldCreateParcelable() {
+        val CREATOR = createCreator(createObject)
+        val createdParcelable = CREATOR.createFromParcel(parcel)
+        assertSame(parcelable, createdParcelable)
+    }
+
+    private class ParcelableImpl : Parcelable {
+        override fun describeContents() = 0
+
+        override fun writeToParcel(dest: Parcel?, flags: Int) {
+        }
+    }
+}
