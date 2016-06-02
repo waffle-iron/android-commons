909efb4daee722d987fe4ea03426e113a56cd49e
Dominik Malantowicz <dominik.malantowicz@gmail.com>
2016-06-02 11:28:45 +0200

    Espresso extensions - onText() functions.

--- espresso/src/main/java/pl/elpassion/android/commons/espresso/ViewInteractions.kt
+++ espresso/src/main/java/pl/elpassion/android/commons/espresso/ViewInteractions.kt
@@ -1,7 +1,13 @@
 package pl.elpassion.android.commons.espresso
 
 import android.support.annotation.IdRes
+import android.support.annotation.StringRes
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.withText
 
 fun onId(@IdRes viewId: Int) = onView(withId(viewId))
+
+fun onText(@StringRes textId: Int) = onView(withText(textId))
+
+fun onText(text: String) = onView(withText(text))
