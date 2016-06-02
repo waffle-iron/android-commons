43705d051328ad997fad9aa050441ec3515c2764
Dominik Malantowicz <dominik.malantowicz@gmail.com>
2016-06-02 11:26:24 +0200

    Espresso extensions  - onId() function.

--- espresso/src/main/java/pl/elpassion/android/commons/espresso/ViewInteractions.kt
+++ espresso/src/main/java/pl/elpassion/android/commons/espresso/ViewInteractions.kt
@@ -1,2 +1,7 @@
 package pl.elpassion.android.commons.espresso
 
+import android.support.annotation.IdRes
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.matcher.ViewMatchers.withId
+
+fun onId(@IdRes viewId: Int) = onView(withId(viewId))
