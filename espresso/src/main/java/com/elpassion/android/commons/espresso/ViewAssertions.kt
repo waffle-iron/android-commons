41ea273ad8f90be916fce35686c78b26073d84ff
Dominik Malantowicz <dominik.malantowicz@gmail.com>
2016-06-03 11:26:55 +0200

    Espresso extensions - isGone() & doesNotExist() functions.

--- espresso/src/main/java/com/elpassion/android/commons/espresso/ViewAssertions.kt
+++ espresso/src/main/java/com/elpassion/android/commons/espresso/ViewAssertions.kt
@@ -1,11 +1,17 @@
 package com.elpassion.android.commons.espresso
 
 import android.support.test.espresso.ViewInteraction
+import android.support.test.espresso.assertion.ViewAssertions
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers
+import android.support.test.espresso.matcher.ViewMatchers.Visibility.GONE
+import android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility
 import org.hamcrest.core.IsNot.not
 
 fun ViewInteraction.isDisplayed() = check(matches(ViewMatchers.isDisplayed()))
 
 fun ViewInteraction.isNotDisplayed() = check(matches(not(ViewMatchers.isDisplayed())))
 
+fun ViewInteraction.isGone() = check(matches(withEffectiveVisibility(GONE)))
+
+fun ViewInteraction.doesNotExist() = check(ViewAssertions.doesNotExist())
