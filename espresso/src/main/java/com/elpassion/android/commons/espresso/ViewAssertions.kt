558643ca415aadb90c981078874aa62949aa982a
Dominik Malantowicz <dominik.malantowicz@gmail.com>
2016-06-03 11:31:32 +0200

    Espresso extensions - hasText() functions.

--- espresso/src/main/java/com/elpassion/android/commons/espresso/ViewAssertions.kt
+++ espresso/src/main/java/com/elpassion/android/commons/espresso/ViewAssertions.kt
@@ -1,11 +1,13 @@
 package com.elpassion.android.commons.espresso
 
+import android.support.annotation.StringRes
 import android.support.test.espresso.ViewInteraction
 import android.support.test.espresso.assertion.ViewAssertions
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers
 import android.support.test.espresso.matcher.ViewMatchers.Visibility.GONE
 import android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility
+import android.support.test.espresso.matcher.ViewMatchers.withText
 import org.hamcrest.core.IsNot.not
 
 fun ViewInteraction.isDisplayed() = check(matches(ViewMatchers.isDisplayed()))
@@ -15,3 +17,7 @@ fun ViewInteraction.isNotDisplayed() = check(matches(not(ViewMatchers.isDisplaye
 fun ViewInteraction.isGone() = check(matches(withEffectiveVisibility(GONE)))
 
 fun ViewInteraction.doesNotExist() = check(ViewAssertions.doesNotExist())
+
+fun ViewInteraction.hasText(@StringRes textId: Int) = check(matches(withText(textId)))
+
+fun ViewInteraction.hasText(text: String) = check(matches(withText(text)))
