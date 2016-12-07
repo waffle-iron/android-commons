d6d18bc5033ca61c3f3b54d848f6261c2dd817b7
Kasper Kondzielski <kasper.kondzielski@elpassion.pl>
2016-12-07 14:44:17 +0100

    Add tests for isVisible and enable/disable functions

--- view/src/androidTest/java/com/elpassion/android/view/ViewTest.kt
+++ view/src/androidTest/java/com/elpassion/android/view/ViewTest.kt
@@ -3,6 +3,7 @@ package com.elpassion.android.view
 import android.app.Application
 import android.test.ApplicationTestCase
 import android.view.View
+import junit.framework.Assert
 
 class ViewTest : ApplicationTestCase<Application>(Application::class.java) {
 
@@ -17,5 +18,34 @@ class ViewTest : ApplicationTestCase<Application>(Application::class.java) {
         val view = View(context)
         view.hide()
         assertEquals(View.GONE, view.visibility)
+    }
+
+    fun testShouldReturnThatViewIsVisibleWhenViewIsVisible() {
+        val view = View(context)
+        assertTrue(view.isVisible())
+    }
+
+    fun testShouldReturnThatViewIsInvisibleWhenIsGone() {
+        val view = View(context)
+        view.visibility = View.GONE
+        assertFalse(view.isVisible())
+    }
+
+    fun testShouldReturnThatViewIsInvisibleWhenIsInvisible() {
+        val view = View(context)
+        view.visibility = View.INVISIBLE
+        assertFalse(view.isVisible())
+    }
+
+    fun testShouldEnableDisabledView() {
+        val view = View(context).apply { isEnabled = false }
+        view.enable()
+        assertTrue(view.isEnabled)
+    }
+
+    fun testShouldDisableEnabledView() {
+        val view = View(context)
+        view.disable()
+        Assert.assertFalse(view.isEnabled)
     }
 }
