efeba46cff6627dca939c42bdac708978beec197
Kasper Kondzielski <kasper.kondzielski@elpassion.pl>
2016-12-07 14:06:16 +0100

    Add rxJava module

--- rxjava/src/main/java/com/example/rxjava/ObservableExtensions.kt
+++ rxjava/src/main/java/com/example/rxjava/ObservableExtensions.kt
@@ -1,2 +1,9 @@
 package com.example.rxjava
 
+import rx.Observable
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
+
+fun <T> Observable<T>.applySchedulers(shouldDelayErrors: Boolean = false) =
+        subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread(), shouldDelayErrors)
