fe93927a22a1e431d08961af776c3f7042c258eb
ghostbuster91 <kghost0@gmail.com>
2016-12-14 00:18:18 +0100

    [Refactoring] Move functions related to stable to single file

--- recycler/src/main/java/com/elpassion/android/commons/recycler/components/stable/StableItemIdStrategy.kt
+++
@@ -1,9 +0,0 @@
-package com.elpassion.android.commons.recycler.components.stable
-
-import android.support.v7.widget.RecyclerView
-import com.elpassion.android.commons.recycler.components.ItemsStrategy
-
-fun <T : StableItemAdapter<out RecyclerView.ViewHolder>> getStableItemIdentifier(itemsStrategy: ItemsStrategy<T>): (Int) -> Long =
-        { position ->
-            itemsStrategy.allItems()[position].stableId
-        }
--- recycler/src/main/java/com/elpassion/android/commons/recycler/components/stable/StableUtils.kt
+++ recycler/src/main/java/com/elpassion/android/commons/recycler/components/stable/StableUtils.kt
@@ -2,6 +2,12 @@ package com.elpassion.android.commons.recycler.components.stable
 
 import android.support.v7.widget.RecyclerView
 import com.elpassion.android.commons.recycler.RecyclerViewAdapterCompositor
+import com.elpassion.android.commons.recycler.components.ItemsStrategy
+
+fun <T : StableItemAdapter<out RecyclerView.ViewHolder>> getStableItemIdentifier(itemsStrategy: ItemsStrategy<T>) =
+        { position: Int ->
+            itemsStrategy.allItems()[position].stableId
+        }
 
 fun <T : StableItemAdapter<out RecyclerView.ViewHolder>> createStableIdInitialization() =
         { compositor: RecyclerViewAdapterCompositor<T> ->
