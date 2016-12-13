80e3398a7aea29d604c4ed6f53423e7a82629d50
ghostbuster91 <kghost0@gmail.com>
2016-12-14 00:06:52 +0100

    Apply CR: apply consistent naming

--- espresso/src/main/java/com/elpassion/android/commons/espresso/matchers/TextInputEditTextHintMatcher.kt
+++ espresso/src/main/java/com/elpassion/android/commons/espresso/matchers/TextInputEditTextHintMatcher.kt
@@ -1,22 +1,23 @@
 package com.elpassion.android.commons.espresso.matchers
 
+import android.support.annotation.StringRes
 import android.support.design.widget.TextInputEditText
 import android.support.design.widget.TextInputLayout
 import android.support.test.espresso.matcher.BoundedMatcher
 import android.view.View
 import org.hamcrest.Description
 
-class TextInputEditTextHintMatcher(private val hintStringId: Int) : BoundedMatcher<View, TextInputEditText>(TextInputEditText::class.java) {
+class TextInputEditTextHintMatcher(@StringRes private val textId: Int) : BoundedMatcher<View, TextInputEditText>(TextInputEditText::class.java) {
 
     override fun matchesSafely(view: TextInputEditText): Boolean {
         val grandpa = view.parent?.parent
         if (grandpa is TextInputLayout) {
-            return grandpa.hint == view.context.getString(hintStringId)
+            return grandpa.hint == view.context.getString(textId)
         }
         return false
     }
 
     override fun describeTo(description: Description) {
-        description.appendText("has hint text from string resource on TextInputLayout: $hintStringId")
+        description.appendText("has hint text from string resource on TextInputLayout: $textId")
     }
 }
