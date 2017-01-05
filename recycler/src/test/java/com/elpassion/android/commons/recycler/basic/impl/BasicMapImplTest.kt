024464cc09cb6aac1a226a765b73b7823ccb3cef
Marek Langiewicz <marek.langiewicz@gmail.com>
2017-01-05 11:45:24 +0100

    Return correct values for one given keys

--- recycler/src/test/java/com/elpassion/android/commons/recycler/basic/impl/BasicMapImplTest.kt
+++ recycler/src/test/java/com/elpassion/android/commons/recycler/basic/impl/BasicMapImplTest.kt
@@ -7,13 +7,15 @@ class BasicMapImplTest {
 
     @Test
     fun shouldCreateBasicMapFromRegularMap(){
-        val basicMap = BasicMapImpl<Int, String?>(mapOf())
+        val basicMap = BasicMapImpl<Int, String>(mapOf())
         assert(basicMap is BasicMap<Int, String?>)
     }
 
     @Test
-    fun shouldReturnCorrectValueForOneGivenKey(){
-        val basicMap = BasicMapImpl<Int, String>(mapOf(1 to "A"))
+    fun shouldReturnCorrectValuesForGivenKeys(){
+        val basicMap = BasicMapImpl<Int, String>(mapOf(1 to "A", 2 to "B", -7 to "ABC"))
         assert(basicMap[1] == "A")
+        assert(basicMap[2] == "B")
+        assert(basicMap[-7] == "ABC")
     }
 }
