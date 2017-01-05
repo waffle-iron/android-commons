6148b1a9ee8bc2cfba60a5a91a4d63beac93f40e
Marek Langiewicz <marek.langiewicz@gmail.com>
2017-01-05 16:15:44 +0100

    [refactoring] basic.Utils: basicMapOf function

--- recycler/src/main/java/com/elpassion/android/commons/recycler/basic/Utils.kt
+++ recycler/src/main/java/com/elpassion/android/commons/recycler/basic/Utils.kt
@@ -5,10 +5,7 @@ import com.elpassion.android.commons.recycler.basic.impl.BasicMapImpl
 
 fun <K, V> Map<K, V>.asBasicMap(): BasicMap<K, V?> = BasicMapImpl(this)
 
-fun <K, V> basicMapOf(vararg entries: Pair<K, V>): BasicMap<K, V?> {
-    val map = mapOf<K, V>(*entries)
-    return map.asBasicMap()
-}
+fun <K, V> basicMapOf(vararg entries: Pair<K, V>) = BasicMapImpl(mapOf<K, V>(*entries))
 
 fun <Item> List<Item>.asBasicList(): BasicList<Item> = BasicListImpl(this)
 
