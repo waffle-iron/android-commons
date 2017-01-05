5ff8850922bda9e01f9bd7589c4ebfe413d75da4
Marcin Suska <marcin.suska@elpassion.pl>
2017-01-05 12:58:17 +0100

    [Refactoring] Remove assert method and replace it by more concrete from Assert package

--- recycler/src/test/java/com/elpassion/android/commons/recycler/basic/impl/BasicMapImplTest.kt
+++ recycler/src/test/java/com/elpassion/android/commons/recycler/basic/impl/BasicMapImplTest.kt
@@ -1,29 +1,34 @@
 package com.elpassion.android.commons.recycler.basic.impl
 
 import com.elpassion.android.commons.recycler.basic.BasicMap
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
 import org.junit.Test
 
 class BasicMapImplTest {
 
     @Test
-    fun shouldCreateBasicMapFromRegularMap(){
+    fun shouldCreateBasicMapFromRegularMap() {
         val basicMap = BasicMapImpl<Int, String>(mapOf())
+
         assert(basicMap is BasicMap<Int, String?>)
     }
 
     @Test
-    fun shouldReturnCorrectValuesForGivenKeys(){
-        val basicMap = BasicMapImpl<Int, String>(mapOf(1 to "A", 2 to "B", -7 to "ABC"))
-        assert(basicMap[1] == "A")
-        assert(basicMap[2] == "B")
-        assert(basicMap[-7] == "ABC")
+    fun shouldReturnCorrectValuesForGivenKeys() {
+        val basicMap = BasicMapImpl(mapOf(1 to "A", 2 to "B", -7 to "ABC"))
+
+        assertEquals(basicMap[1], "A")
+        assertEquals(basicMap[2], "B")
+        assertEquals(basicMap[-7], "ABC")
     }
 
     @Test
-    fun shouldReturnNullForNonExistingValues(){
-        val basicMap = BasicMapImpl<Int, String>(mapOf(1 to "A", 2 to "B", -7 to "ABC"))
-        assert(basicMap[-1] === null)
-        assert(basicMap[3] === null)
-        assert(basicMap[4] === null)
+    fun shouldReturnNullForNonExistingValues() {
+        val basicMap = BasicMapImpl(mapOf(1 to "A", 2 to "B", -7 to "ABC"))
+
+        assertNull(basicMap[-1])
+        assertNull(basicMap[3])
+        assertNull(basicMap[4])
     }
 }
