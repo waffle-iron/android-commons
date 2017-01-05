cd9c5d99b1555eb4ac82f1f9e12407d38723def9
Marek Langiewicz <marek.langiewicz@gmail.com>
2017-01-05 13:46:30 +0100

    BasicListImpl: Return correct values for given positions

--- recycler/src/main/java/com/elpassion/android/commons/recycler/basic/impl/BasicListImpl.kt
+++ recycler/src/main/java/com/elpassion/android/commons/recycler/basic/impl/BasicListImpl.kt
@@ -4,7 +4,7 @@ import com.elpassion.android.commons.recycler.basic.BasicList
 
 class BasicListImpl<Item>(private val source: List<Item>) : BasicList<Item> {
 
-    override fun get(key: Int) = throw NotImplementedError()
+    override fun get(key: Int) = source[key]
 
     override val size: Int get() = throw NotImplementedError()
 }
--- recycler/src/test/java/com/elpassion/android/commons/recycler/basic/impl/BasicListImplTest.kt
+++ recycler/src/test/java/com/elpassion/android/commons/recycler/basic/impl/BasicListImplTest.kt
@@ -1,6 +1,7 @@
 package com.elpassion.android.commons.recycler.basic.impl
 
 import com.elpassion.android.commons.recycler.basic.BasicList
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class BasicListImplTest {
@@ -10,5 +11,14 @@ class BasicListImplTest {
         val basicList = BasicListImpl<String>(listOf())
 
         assert(basicList is BasicList<String>)
+    }
+
+    @Test
+    fun shouldReturnCorrectValuesForGivenPositions() {
+        val basicList = BasicListImpl(listOf("A", "B", "ABC"))
+
+        assertEquals(basicList[0], "A")
+        assertEquals(basicList[1], "B")
+        assertEquals(basicList[2], "ABC")
     }
 }
