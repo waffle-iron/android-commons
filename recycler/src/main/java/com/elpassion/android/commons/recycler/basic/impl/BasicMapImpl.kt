29c1ca21f5c1a39b38f24d4339cff4ec2b6e3b5c
Marek Langiewicz <marek.langiewicz@gmail.com>
2017-01-05 11:40:12 +0100

    Return correct value for one given key

--- recycler/src/main/java/com/elpassion/android/commons/recycler/basic/impl/BasicMapImpl.kt
+++ recycler/src/main/java/com/elpassion/android/commons/recycler/basic/impl/BasicMapImpl.kt
@@ -2,8 +2,6 @@ package com.elpassion.android.commons.recycler.basic.impl
 
 import com.elpassion.android.commons.recycler.basic.BasicMap
 
-class BasicMapImpl<Key, Value>(val source: Map<Any, Any>) : BasicMap<Key, Value> {
-    override fun get(key: Key): Value {
-        throw NotImplementedError()
-    }
+class BasicMapImpl<Key, Value>(val source: Map<Key, Value>) : BasicMap<Key, Value?> {
+    override fun get(key: Key): Value? = source[key]
 }
--- recycler/src/test/java/com/elpassion/android/commons/recycler/basic/impl/BasicMapImplTest.kt
+++ recycler/src/test/java/com/elpassion/android/commons/recycler/basic/impl/BasicMapImplTest.kt
@@ -7,7 +7,13 @@ class BasicMapImplTest {
 
     @Test
     fun shouldCreateBasicMapFromRegularMap(){
-        val basicMap = BasicMapImpl<Int, String>(mapOf())
-        assert(basicMap is BasicMap<Int, String>)
+        val basicMap = BasicMapImpl<Int, String?>(mapOf())
+        assert(basicMap is BasicMap<Int, String?>)
+    }
+
+    @Test
+    fun shouldReturnCorrectValueForOneGivenKey(){
+        val basicMap = BasicMapImpl<Int, String>(mapOf(1 to "A"))
+        assert(basicMap[1] == "A")
     }
 }
