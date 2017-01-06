ec4c1423e0ea8d4f2d257e62e776193023074136
Marek Langiewicz <marek.langiewicz@gmail.com>
2017-01-06 15:49:47 +0100

    [Refactoring] Rename param in basicMapOf function

--- recycler/src/main/java/com/elpassion/android/commons/recycler/basic/Utils.kt
+++ recycler/src/main/java/com/elpassion/android/commons/recycler/basic/Utils.kt
@@ -5,7 +5,7 @@ import com.elpassion.android.commons.recycler.basic.impl.BasicMapImpl
 
 fun <K, V> Map<K, V>.asBasicMap(): BasicMap<K, V?> = BasicMapImpl(this)
 
-fun <K, V> basicMapOf(vararg entries: Pair<K, V>) = BasicMapImpl(mapOf<K, V>(*entries))
+fun <K, V> basicMapOf(vararg pairs: Pair<K, V>) = BasicMapImpl(mapOf<K, V>(*pairs))
 
 fun <Item> List<Item>.asBasicList(): BasicList<Item> = BasicListImpl(this)
 
