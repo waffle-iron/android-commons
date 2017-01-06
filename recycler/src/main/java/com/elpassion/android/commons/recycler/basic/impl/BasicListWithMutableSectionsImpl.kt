2d2ba3d0795e980090e8707ce51676f522a6356a
Marek Langiewicz <marek.langiewicz@gmail.com>
2017-01-06 22:30:45 +0100

    BasicListWithMutableSections: Behave like BasicListWithSections for read only operations

---
+++ recycler/src/main/java/com/elpassion/android/commons/recycler/basic/impl/BasicListWithMutableSectionsImpl.kt
@@ -0,0 +1,25 @@
+package com.elpassion.android.commons.recycler.basic.impl
+
+import com.elpassion.android.commons.recycler.basic.BasicListWithMutableSections
+import com.elpassion.android.commons.recycler.basic.BasicMutableList
+import com.elpassion.android.commons.recycler.basic.BasicMutableMap
+import com.elpassion.android.commons.recycler.basic.asBasicMutableMap
+
+class BasicListWithMutableSectionsImpl<Item, Section>(private val source: MutableMap<Section, BasicMutableList<Item>>) : BasicListWithMutableSections<Item, Section> {
+
+    override val sections = source.asBasicMutableMap()
+
+    override fun get(key: Int): Item {
+        var offset = 0
+        for (section in source.values) {
+            if (key < offset + section.size) {
+                return section[key - offset]
+            } else {
+                offset += section.size
+            }
+        }
+        throw IndexOutOfBoundsException()
+    }
+
+    override val size: Int get() = source.map { entry -> entry.value.size }.sum()
+}
