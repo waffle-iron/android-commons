1e846426529695333e04af9f7d0bbc12f581d241
Marek Langiewicz <marek.langiewicz@gmail.com>
2017-01-16 19:17:38 +0100

    Add Observable.catch operator to RxUtils

--- rxjava/src/main/java/com/elpassion/android/commons/rxjava/RxUtils.kt
+++ rxjava/src/main/java/com/elpassion/android/commons/rxjava/RxUtils.kt
@@ -1,8 +1,18 @@
 package com.elpassion.android.commons.rxjava
 
+import rx.Observable
 import rx.Subscription
 import rx.subscriptions.CompositeSubscription
 
 fun Subscription.addTo(subscription: CompositeSubscription) = subscription.add(this)
 
+inline fun <reified Ex : Throwable> Observable<*>.catch(crossinline action: (throwable: Ex) -> Unit) = this
+        .onErrorResumeNext {
+            if (it is Ex) {
+                action(it)
+                Observable.empty()
+            } else {
+                Observable.error(it)
+            }
+        }
 
--- rxjava/src/test/java/com/elpassion/android/commons/rxjava/RxUtilsTest.kt
+++ rxjava/src/test/java/com/elpassion/android/commons/rxjava/RxUtilsTest.kt
@@ -1,23 +1,31 @@
 package com.elpassion.android.commons.rxjava
 
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.times
+import com.nhaarman.mockito_kotlin.verify
 import org.junit.Test
+import rx.exceptions.OnErrorNotImplementedException
+import rx.observers.TestSubscriber
 import rx.subjects.PublishSubject
 import rx.subscriptions.CompositeSubscription
 
 class RxUtilsTest {
 
-    val subject = PublishSubject.create<Unit>()
+    private val subject = PublishSubject.create<Unit>()
 
-    val compositeSubscription = CompositeSubscription()
+    private val subscription = CompositeSubscription()
+
+    private val handler: (Throwable) -> Unit = mock()
 
     @Test
     fun shouldAddSubscriptionToCompositeSubscription() {
 
         subject
                 .subscribe()
-                .addTo(compositeSubscription)
+                .addTo(subscription)
 
-        assert(compositeSubscription.hasSubscriptions())
+        assert(subscription.hasSubscriptions())
     }
 
     @Test
@@ -25,12 +33,57 @@ class RxUtilsTest {
 
         subject
                 .subscribe()
-                .addTo(compositeSubscription)
+                .addTo(subscription)
 
         assert(subject.hasObservers())
 
-        compositeSubscription.clear()
+        subscription.clear()
 
         assert(!subject.hasObservers())
+    }
+
+    @Test
+    fun shouldCatchRxError() {
+
+        subject
+                .catch<IllegalArgumentException> {
+                    handler(it)
+                }
+                .subscribe()
+
+        val exception = IllegalArgumentException()
+
+        subject.onError(exception)
+
+        verify(handler).invoke(exception)
+    }
+
+    @Test(expected = OnErrorNotImplementedException::class)
+    fun shouldNotCatchRxError() {
+
+        subject
+                .catch<IllegalStateException> {
+                    handler(it)
+                }
+                .subscribe()
+
+        subject.onError(IllegalArgumentException())
+
+        verify(handler, times(0)).invoke(any())
+    }
+
+    @Test
+    fun shouldPassItemsThrough() {
+
+        val subscriber = TestSubscriber<Any>()
+
+        subject
+                .catch<IllegalArgumentException> { }
+                .subscribe(subscriber)
+
+        subject.onNext(Unit)
+        subject.onNext(Unit)
+
+        subscriber.assertValues(Unit, Unit)
     }
 }
