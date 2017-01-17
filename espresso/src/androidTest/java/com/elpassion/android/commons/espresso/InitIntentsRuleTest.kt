aca27e7f0d6d2cd7fa652a8784770a2d374a77ca
Karol Kowalski <ordon1313@gmail.com>
2017-01-17 17:13:02 +0100

    Add InitIntentsRuleTest

---
+++ espresso/src/androidTest/java/com/elpassion/android/commons/espresso/InitIntentsRuleTest.kt
@@ -0,0 +1,37 @@
+package com.elpassion.android.commons.espresso
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.test.rule.ActivityTestRule
+import android.widget.FrameLayout
+import org.junit.Rule
+import org.junit.Test
+
+class InitIntentsRuleTest {
+
+    @JvmField @Rule
+    val activityRule = ActivityTestRule(Activity::class.java)
+
+    @JvmField @Rule
+    val intentsRule = InitIntentsRule()
+
+    @Test
+    fun shouldCheckIntent() {
+        checkIntent(StartingActivity::class.java)
+    }
+
+    class Activity : android.app.Activity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(FrameLayout(this))
+            startActivity(Intent(this, StartingActivity::class.java))
+        }
+    }
+
+    class StartingActivity : android.app.Activity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(FrameLayout(this))
+        }
+    }
+}
