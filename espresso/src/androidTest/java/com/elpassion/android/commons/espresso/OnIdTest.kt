670a70056f591a6891326fae217fcf67be86824c
Karol Kowalski <ordon1313@gmail.com>
2017-01-17 17:49:14 +0100

    Add OnIdTest

---
+++ espresso/src/androidTest/java/com/elpassion/android/commons/espresso/OnIdTest.kt
@@ -0,0 +1,40 @@
+package com.elpassion.android.commons.espresso
+
+import android.os.Bundle
+import android.support.test.rule.ActivityTestRule
+import android.widget.Button
+import android.widget.FrameLayout
+import org.junit.Rule
+import org.junit.Test
+
+class OnIdTest {
+
+    @JvmField @Rule
+    val activityRule = ActivityTestRule(Activity::class.java)
+
+    @Test
+    fun shouldNotFindViewById() {
+        onId(notExistingId).doesNotExist()
+    }
+
+    @Test
+    fun shouldFindViewById() {
+        onId(existingId).isDisplayed()
+    }
+
+    class Activity : android.app.Activity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(FrameLayout(this).apply {
+                addView(Button(this.context).apply {
+                    id = existingId
+                })
+            })
+        }
+    }
+
+    companion object {
+        private val notExistingId = 1234
+        private val existingId = 123
+    }
+}
