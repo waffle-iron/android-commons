331ce227d28b0b514aa3323f0a7e80f8a8247db1
Maciej Górski <maciek.gorski@gmail.com>
2017-01-19 15:27:15 +0100

    Add 'should not be gone when parent invisible' test.

--- espresso/src/androidTest/java/com/elpassion/android/commons/espresso/ParentGoneChildVisiblityAssertionTest.kt
+++ espresso/src/androidTest/java/com/elpassion/android/commons/espresso/ParentGoneChildVisiblityAssertionTest.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.FrameLayout
 import com.elpassion.android.commons.espresso.test.R
+import junit.framework.AssertionFailedError
 import org.junit.Rule
 import org.junit.Test
 
@@ -19,14 +20,28 @@ class ParentGoneChildVisiblityAssertionTest {
         onId(R.id.first).isGone()
     }
 
+    @Test(expected = AssertionFailedError::class)
+    fun shouldNotBeGoneWhenParentInvisible() {
+        onId(R.id.second).isGone()
+    }
+
     class Activity : android.app.Activity() {
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             setContentView(FrameLayout(this).apply {
-                visibility = View.GONE
-                addView(Button(this.context).apply {
-                    id = R.id.first
-                    visibility = View.VISIBLE
+                addView(FrameLayout(context).apply {
+                    visibility = View.GONE
+                    addView(Button(context).apply {
+                        id = R.id.first
+                        visibility = View.VISIBLE
+                    })
+                })
+                addView(FrameLayout(context).apply {
+                    visibility = View.INVISIBLE
+                    addView(Button(context).apply {
+                        id = R.id.second
+                        visibility = View.VISIBLE
+                    })
                 })
             })
         }
