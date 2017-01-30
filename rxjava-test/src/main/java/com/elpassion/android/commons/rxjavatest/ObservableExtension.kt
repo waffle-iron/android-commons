4077aa31fa216a6cc41e46fe93421733715bb046
Marcin Suska <marcin.suska@elpassion.pl>
2017-01-30 18:46:26 +0100

    Add extension method test() in Observable to easier test chain method connected with Observable. This action is similar to RxJava 2.

--- rxjava-test/src/main/java/com/elpassion/android/commons/rxjavatest/ObservableExtension.kt
+++ rxjava-test/src/main/java/com/elpassion/android/commons/rxjavatest/ObservableExtension.kt
@@ -2,9 +2,22 @@ package com.elpassion.android.commons.rxjavatest
 
 import org.mockito.stubbing.OngoingStubbing
 import rx.Observable
+import rx.observers.TestSubscriber
 
 fun <T> OngoingStubbing<Observable<T>>.thenNeverending(): OngoingStubbing<Observable<T>> = thenReturn(Observable.never())
 
 fun <T> OngoingStubbing<Observable<T>>.thenError(exception: Exception): OngoingStubbing<Observable<T>> = thenReturn(Observable.error(exception))
 
 fun <T> OngoingStubbing<Observable<T>>.thenJust(response: T): OngoingStubbing<Observable<T>> = thenReturn(Observable.just(response))
+
+fun <T> Observable<T>.test(): TestSubscriber<T> {
+    val subscriber = TestSubscriber<T>()
+    subscribe(subscriber)
+    return subscriber
+}
+
+fun <T> Observable<T>.test(assertion: TestSubscriber<T>.() -> Unit) {
+    val subscriber = TestSubscriber<T>()
+    subscribe(subscriber)
+    subscriber.assertion()
+}
