92fd49fcf471c3f52858c40922380d5ce9a8ab95
Marcin Suska <marcin.suska@elpassion.pl>
2017-01-30 18:57:58 +0100

    Add test extension method to completable

--- rxjava-test/src/main/java/com/elpassion/android/commons/rxjavatest/CompletableExtension.kt
+++ rxjava-test/src/main/java/com/elpassion/android/commons/rxjavatest/CompletableExtension.kt
@@ -2,9 +2,22 @@ package com.elpassion.android.commons.rxjavatest
 
 import org.mockito.stubbing.OngoingStubbing
 import rx.Completable
+import rx.observers.TestSubscriber
 
 fun OngoingStubbing<Completable>.thenNeverending() = thenReturn(Completable.never())
 
 fun OngoingStubbing<Completable>.thenError(exception: Exception) = thenReturn(Completable.error(exception))
 
 fun OngoingStubbing<Completable>.thenComplete() = thenReturn(Completable.complete())
+
+fun Completable.test(): TestSubscriber<Unit> {
+    val subscriber = TestSubscriber<Unit>()
+    subscribe(subscriber)
+    return subscriber
+}
+
+fun Completable.test(assertion: TestSubscriber<Unit>.() -> Unit) {
+    val subscriber = TestSubscriber<Unit>()
+    subscribe(subscriber)
+    subscriber.assertion()
+}
