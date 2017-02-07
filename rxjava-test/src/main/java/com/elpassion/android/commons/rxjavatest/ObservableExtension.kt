a85202473c61e90a978f813c90e6d941010b45ee
Marcin Suska <marcin.suska@elpassion.pl>
2017-02-07 14:23:42 +0100

    Add tested extensions for Observable stubbing

--- rxjava-test/src/main/java/com/elpassion/android/commons/rxjavatest/ObservableExtension.kt
+++ rxjava-test/src/main/java/com/elpassion/android/commons/rxjavatest/ObservableExtension.kt
@@ -1,11 +1,21 @@
 package com.elpassion.android.commons.rxjavatest
 
-import org.mockito.stubbing.OngoingStubbing
+import com.nhaarman.mockito_kotlin.doReturn
+ import org.mockito.stubbing.OngoingStubbing
 import rx.Observable
 import rx.observers.TestSubscriber
 
-//TODO: CR: Restore methods removed in 2e64b5601019633a6a11e750c6e222c489356c76 in TDD
 fun <T> OngoingStubbing<Observable<T>>.thenNever(): OngoingStubbing<Observable<T>> = thenReturn(Observable.never())
+
+fun <T> OngoingStubbing<Observable<T>>.thenJust(value: T): OngoingStubbing<Observable<T>> = thenReturn(Observable.just(value))
+
+fun <T> OngoingStubbing<Observable<T>>.thenError(exception: Exception): OngoingStubbing<Observable<T>> = thenReturn(Observable.error(exception))
+
+fun <T> OngoingStubbing<Observable<T>>.doReturnJust(value: T) = doReturn(Observable.just(value))
+
+fun <T> OngoingStubbing<Observable<T>>.doReturnNever() = doReturn(Observable.never())
+
+fun <T> OngoingStubbing<Observable<T>>.doReturnError(exception: Exception) = doReturn(Observable.error(exception))
 
 fun <T> Observable<T>.test() = TestSubscriber<T>().apply { subscribe(this) }
 
